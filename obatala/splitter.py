f98a8db6a75d757b3b861f91a86c990cef4e099f
adxpillar <dsa.adeagbo@gmail.com>
2020-04-08 16:21:57 -0400

    update module conditional

--- obatala/splitter.py
+++ obatala/splitter.py
@@ -6,12 +6,7 @@ def spltr(X):
     Function will split dates ("MM/DD/YYYY")
     into multiple columns 
     """
-    X = X.copy()
-    X['Date'] = pandas.to_datetime(X['Date'], infer_datetime_format=True)
-    X['Year'] = X['Date'].dt.year 
-    X['Month'] = X['Date'].dt.month
-    X['Day'] = X['Date'].dt.day
-    X = X.drop(columns='Date')
+    
 
 
     return X
@@ -19,6 +14,12 @@ def spltr(X):
 # d = splitter(df)
 # print(d)
 if __name__ == "__main__":
+     X = X.copy()
+    X['Date'] = pandas.to_datetime(X['Date'], infer_datetime_format=True)
+    X['Year'] = X['Date'].dt.year 
+    X['Month'] = X['Date'].dt.month
+    X['Day'] = X['Date'].dt.day
+    X = X.drop(columns='Date')
     df = spltr(df)
     print(df.head())
 
